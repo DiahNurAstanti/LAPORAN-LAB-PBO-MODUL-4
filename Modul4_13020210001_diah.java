5f553ec8a19d930b08c6085e6a1f390ddef96abb
DIAH NUR ASTANTI <127530115+DiahNurAstanti@users.noreply.github.com>
2023-04-26 11:16:25 +0700

    Update Modul4_13020210001_diah.java

--- Modul4_13020210001_diah.java
+++ Modul4_13020210001_diah.java
@@ -1,3 +1,6 @@
+SOAL:
+Terapkan akses modifier pada atribut dan lakukan proses enkapsulasi pada suatu kelas dan juga Implementasikan method setter dan getter pada attribute kelas yang sudah diberikan akses modifier private
+
 Class:
 public class Mobil {
     private String merk;
